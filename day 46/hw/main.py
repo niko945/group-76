8b779cfffcd051d7ea2c205c3230388be4b9ad86
niko945 <nikamdivani11@gmail.com>
2025-12-20 18:51:17 +0400

    DDD

--- day 46/hw/main.py
+++ day 46/hw/main.py
@@ -1,35 +1,32 @@
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-to_remove = []
+# to_remove = []
 
-for i in range(len(nums)):
-    if nums[i] % 2 == 0 or i % 2 == 1:
-        to_remove.append(nums[i])
+# for i in range(len(nums)):
+#     if nums[i] % 2 == 0 or i % 2 == 1:
+#         to_remove.append(nums[i])
 
-for x in to_remove:
-    nums.remove(x)
+# for x in to_remove:
+#     nums.remove(x)
 
-print(nums)
-
-
-words = ["sun", "moon", "sky"]
-
-copy_words = words.()
-
-for w in copy_words:
-    words.append(w) 
-
-print(words)
+# print(nums)
 
 
-str_list = ["apple", "banana"]
-num_list = [10, 25, 30, 55, 45, 5]
 
-for n in num_list:
-    if 20 < n < 50:
-        str_list.append(str(n))
+# str_list = ["apple", "banana"]
+# num_list = [10, 25, 30, 55, 45, 5]
 
-print(str_list)
+# for n in num_list:
+#     if 20 < n < 50:
+#         str_list.append(str(n))
+
+# print(str_list)
 
 
+
+# 4) შექმენით სიტყვებით სავსე სია და ამ სიაში ყველა ისეთ სიტყვას რომელიც 
+# იწყება პატარა ასოთი, პირველი ასო გაუხადეთ დიდი. გამოიყენეთ for ციკლი 
+# და სტრინგის შესაბამისი ფუნქცია.
+
+lis = ["Nika", "Dadu", "nika", "dadu"]
 
