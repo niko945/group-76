24242b34e2a24585c399f3fe17600bef9c6a913e
niko945 <nikamdivani11@gmail.com>
2025-12-27 19:35:58 +0400

    EFEFEFF

---
+++ day 50/cw/main.py
@@ -0,0 +1,15 @@
+# lis = ["Html", "Python", "CSS", "java", "javascript"]
+# new_lis = []
+# for i in lis:
+#     if len(i) <= 4:
+#         new_lis.append(i)
+# print(new_lis)
+
+
+lis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum = 0
+amount = 0
+for i in lis:
+    sum += i
+    amount += 1
+print(sum // amount)
