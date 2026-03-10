81ec03f4526b3f54afa51a6b56a79f9f59eea919
niko945 <nikamdivani11@gmail.com>
2026-03-10 16:23:59 +0400

    EFEEF

---
+++ day 69/hw/main.py
@@ -0,0 +1,21 @@
+# def solution(str):
+#     return str[::-1]
+
+# def positive_sum(arr):
+#     sum = 0
+#     for i in arr:
+#         if i > 0:
+#             sum = sum + i
+#     return sum
+
+# def repeat_str(repeat, string):
+#     return repeat * string
+
+# def square_sum(numbers):
+# 	res = 0
+# 	for num in numbers:
+#    		res = res + num*num
+# 	return res
+
+# def string_to_number(s):
+#     return int(s)
---
+++ day 70/hw/main.py
@@ -0,0 +1,34 @@
+# def greet():
+#     return "hello world!"
+
+# def summation(num):
+#     total = 0
+#     for i in range(1, num + 1):
+#         total += i
+#     return total
+
+# def sum_array(arr):
+#     return sum(arr)
+
+# def digitize(n):
+#     result = []
+#     for d in str(n):
+#         result.append(int(d))
+#     result.reverse()
+#     return result
+
+# def are_you_playing_banjo(name):
+#     if name[0].lower() == 'r':
+#         return name + " plays banjo"
+#     else:
+#         return name + " does not play banjo"
+
+# def abbrev_name(name):
+#     first, last = name.split()
+#     return first[0].upper() + "." + last[0].upper()
+
+# def find_average(numbers):
+#     if len(numbers) == 0:
+#         return 0
+#     return sum(numbers) / len(numbers)
+
