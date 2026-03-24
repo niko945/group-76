6c468e8d2f2ae9e3d0cf02ba9dcce1dd067916db
niko945 <nikamdivani11@gmail.com>
2026-03-24 19:50:08 +0400

    FRFR

--- day 76/hw/main.py
+++ day 76/hw/main.py
@@ -4,11 +4,3 @@ def dont_give_me_five(start, end):
         if '5' not in str(i):
             count += 1
     return count
-
-
-def number(bus_stops):
-    people = 0
-    for i in bus_stops:
-        people += i[0]
-        people -= i[1]
-    return people
