ec2c6f69f882b904fa4010425b76c5a2187ec5c3
Nikoloz Mdivani <nikamdivani11@gmail.com>
2026-07-20 22:28:52 +0400

    1

---
+++ day 109/hw/main.py
@@ -0,0 +1,17 @@
+# def add_binary(a, b):
+#     n = a + b
+#     result = ""
+
+#     if n == 0:
+#         return "0"
+
+#     while n > 0:
+#         result = str(n % 2) + result
+#         n //= 2
+
+#     return result
+
+# def interlockable(a, b):
+#     return (a & b) == 0
+
+
